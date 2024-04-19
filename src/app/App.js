cbc10ff0cfa5278af5a3ac273fcccabcdf64701c
sh-navid <shamshirsaz.navid@live.com>
2024-04-19 11:44:52 +0330

    Setup app

---
+++ src/app/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+{
+  /* <img src={logo} /> */
+}
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <span>Game</span>
+    </>
+  );
+}
+
+export default App;
